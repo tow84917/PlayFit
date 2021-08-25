14f4d015c78f9b662ec30d6559bab8031959bb64
tow84917 <tow84917@gmail.com>
2021-08-25 11:16:37 +0800

    add login/signup page

---
+++ src/main/resources/templates/login_signup/login.js
@@ -0,0 +1,44 @@
+window.addEventListener('load',doFirst);
+
+position = sessionStorage.position;
+
+function doFirst(){     
+
+    //初始化
+    if(!position){
+        position="left";
+    }  
+    if(position =="right"){
+        swiftBox.style.transform="translate(100%,0)";
+        // swiftBox.style.transition="none";
+        swiftButton.innerText = `LOG IN`;
+        changeTitle.innerHTML = `WELCOME BACK! <br> GORGEOUS!`;
+        changeText.innerText=`Play more, fit more!`;
+    }
+    swiftButton = document.getElementById('swiftButton');
+    swiftButton.addEventListener('click',move);
+}
+
+
+function move(){
+    changeTitle = document.getElementById('changeTitle');
+    changeText = document.getElementById('changeText');
+    swiftBox = document.getElementById('swiftBox');
+
+    if(position == "left"){
+    swiftBox.style.transform="translate(100%,0)";
+    swiftButton.innerText = `LOG IN`;
+    changeTitle.innerHTML = `WELCOME BACK! <br> GORGEOUS!`;
+    changeText.innerText=`Play more, fit more!`;
+    position = "right";
+    sessionStorage.position= position;
+    }else{
+    swiftBox.style.transform="translate(0,0)";
+    swiftButton.innerText = `SIGN UP`;
+    changeTitle.innerHTML = `HELLO,<br>NEW-<br>COMER!`;
+    changeText.innerHTML=`Need more exercises?<br>Come join us!`;
+    position = "left";
+    sessionStorage.position= position;
+    }
+
+}
