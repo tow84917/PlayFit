9f38abc587b87866fd428f9663da8575ace4b4e9
FrankHsiao <lordarcher12345@gmail.com>
2021-09-01 11:52:12 +0800

    .

--- src/main/java/com/java016/playfit/security/SecurityConfiguration.java
+++ src/main/java/com/java016/playfit/security/SecurityConfiguration.java
@@ -69,8 +69,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 				.formLogin()
 				.usernameParameter("email")
 				.loginPage("/login")
-				.failureHandler(customAuthenticationFailureHandler()) //失敗處理,認證信後改
-//				.failureUrl("/login?error=true") // 回傳有誤
+//				.failureHandler(customAuthenticationFailureHandler()) //失敗處理,認證信後改
+				.failureUrl("/login?error=true") // 回傳有誤
 				.defaultSuccessUrl("/") // 回到首頁 或 跳轉原拜訪頁
 				.permitAll()
 				.and()
