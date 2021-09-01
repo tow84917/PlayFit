6b394c6e18a7cea19eaed9912803a68e526dc92f
tow84917 <tow84917@gmail.com>
2021-09-01 22:52:27 +0800

    temp sign up form step3(create avatar) html,css,images

--- src/main/java/com/java016/playfit/security/SecurityConfiguration.java
+++ src/main/java/com/java016/playfit/security/SecurityConfiguration.java
@@ -63,7 +63,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
 				.antMatchers("/", "/process_register" ,"/login/failure" 
-						, "/register","/**/*.js", "/**/*.css").permitAll() // void not css、html 
+						, "/register","/**/*.js", "/**/*.css", "/**/*.svg").permitAll() // void not css、html 
 				.anyRequest().authenticated() // 除了上行請求皆須登入
 				.and()
 				.formLogin()
