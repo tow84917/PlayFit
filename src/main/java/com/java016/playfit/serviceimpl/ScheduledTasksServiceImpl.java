a841b5f724f7ca1b7fea3e241dafd14db342044c
FrankHsiao <lordarcher12345@gmail.com>
2021-09-01 16:11:08 +0800

    給測試帳號較完整資料

--- src/main/java/com/java016/playfit/serviceimpl/ScheduledTasksServiceImpl.java
+++ src/main/java/com/java016/playfit/serviceimpl/ScheduledTasksServiceImpl.java
@@ -31,7 +31,7 @@ public class ScheduledTasksServiceImpl implements ScheduledTasksService {
 	BodyCalculator bodyCalculator;
 
 	@Override
-	@Scheduled(cron = "10 06 17 * * ?") // 指定時間執行 0時(24)
+	@Scheduled(cron = "0 0 0 * * ?") // 指定時間執行 0時(24)
 	public void upadteCalorieDeficit() {
 
 		long now = System.currentTimeMillis();
