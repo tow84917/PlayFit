a8dcaa01b03727a0fa986493471011d1d80df47b
wangtc07 <as55888d@gmail.com>
2021-09-03 04:26:50 +0800

    order record 測試

---
+++ src/main/java/com/java016/playfit/dao/OrderRecordRepository.java
@@ -0,0 +1,9 @@
+package com.java016.playfit.dao;
+
+import com.java016.playfit.model.OrderRecord;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRecordRepository extends JpaRepository<OrderRecord, Integer> {
+
+
+}
