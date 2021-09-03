61b82d288f3b60b808b5835e6be40dda2124e88c
wangtc07 <as55888d@gmail.com>
2021-09-03 13:17:21 +0800

    返回給前端交易結果資訊

--- src/main/java/com/java016/playfit/controller/Pay.java
+++ src/main/java/com/java016/playfit/controller/Pay.java
@@ -1,9 +1,13 @@
 package com.java016.playfit.controller;
 
+import com.java016.playfit.model.OrderRecord;
+import com.java016.playfit.model.User;
+import com.java016.playfit.service.UserService;
 import ecpay.payment.integration.AllInOne;
 import example.ExampleAllInOne;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,12 +16,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
 import java.util.Map;
 
 @Controller
 public class Pay {
     private static final Logger logger = LogManager.getLogger(Pay.class);
     public static AllInOne all;
+
+    @Autowired
+    UserService userService;
 
     public Pay() {
     }
@@ -103,41 +115,80 @@ public class Pay {
     public String payFinish(@RequestParam Map<String,Object> paramsMap ,
                             Model model) {
     	logger.info("payFinish-------->>");
-    	if (paramsMap.get("RtnCode") == "1"){ // 交易成功
+    	Integer RtnCode = Integer.parseInt((String)paramsMap.get("RtnCode"));
+    	logger.info(RtnCode);
+    	if (RtnCode == 1){ // 交易成功
+    		logger.info("交易成功 payFinish-------->>");
+    		model.addAttribute("msg", "交易成功");
+            OrderRecord orderRecord = new OrderRecord();
+            Date date;
+            Calendar calendar;
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
             for (String s : paramsMap.keySet()) {
                 System.out.println("----------------");
                 System.out.println(s);
                 System.out.println(paramsMap.get(s));
                 System.out.println("----------------");
-
                 switch (s){
                     case "MerchantTradeNo": // 訂單編號
-                        model.addAttribute("MerchantTradeNo", paramsMap.get(s));
+//                        model.addAttribute("MerchantTradeNo", paramsMap.get(s));
+                        orderRecord.setMerchantTradeNo((String)paramsMap.get(s));
                         break;
                     case "PaymentDate": // 付款時間
-                        model.addAttribute("PaymentDate", paramsMap.get(s));
+//                        model.addAttribute("PaymentDate", paramsMap.get(s));
+                        try {
+                            date = dateFormat.parse((String)paramsMap.get(s));
+                        } catch (ParseException e) {
+                            e.printStackTrace();
+                            break;
+                        }
+                        calendar = Calendar.getInstance();
+                        calendar.setTime(date);
+                        orderRecord.setPaymentDate(calendar);
                         break;
                     case "PaymentType": // 付款方式
-                        model.addAttribute("PaymentType", paramsMap.get(s));
+//                        model.addAttribute("PaymentType", paramsMap.get(s));
+                        orderRecord.setPaymentType((String)paramsMap.get(s));
                         break;
                     case "PaymentTypeChargeFee": // 手續費
-                        model.addAttribute("PaymentTypeChargeFee", paramsMap.get(s));
+//                        model.addAttribute("PaymentTypeChargeFee", paramsMap.get(s));
+                        orderRecord.setPaymentTypeChargeFee(Integer.parseInt((String)paramsMap.get(s)));
                         break;
                     case "TradeAmt": // 交易金額
-                        model.addAttribute("TradeAmt", paramsMap.get(s));
+//                        model.addAttribute("TradeAmt", paramsMap.get(s));
+                        orderRecord.setTradeAmt(Integer.parseInt((String)paramsMap.get(s)));
                         break;
                     case "TradeDate": // 訂單成立時間
-                        model.addAttribute("TradeDate", paramsMap.get(s));
+//                        model.addAttribute("TradeDate", paramsMap.get(s));
+
+                        try {
+                            date = dateFormat.parse((String)paramsMap.get(s));
+                        } catch (ParseException e) {
+                            e.printStackTrace();
+                            break;
+                        }
+                        calendar = Calendar.getInstance();
+                        calendar.setTime(date);
+                        orderRecord.setTradeDate(calendar);
                         break;
                     case "TradeNo": // 綠界的交易編號
-                        model.addAttribute("TradeNo", paramsMap.get(s));
+//                        model.addAttribute("TradeNo", paramsMap.get(s));
+                        orderRecord.setTradeNo((String)paramsMap.get(s));
                         break;
                 }
             }
+//            User loginUser = userService.getLoginUser();
+//            orderRecord.setUserId(loginUser);
+            logger.info(orderRecord);
+            System.out.println(orderRecord);
+            model.addAttribute("RenMsg" , orderRecord);
 
         } else {
     	    // 交易失敗
-            Object rtnMsg = paramsMap.get("RtnMsg");
+        	logger.info("交易失敗 payFinish-------->>");
+        	model.addAttribute("msg", "交易失敗");
+            String rtnMsg = (String)paramsMap.get("RtnMsg");
+            System.out.println(rtnMsg);
             model.addAttribute("RtnMsg", rtnMsg);
         }
     	return "payFinish";
