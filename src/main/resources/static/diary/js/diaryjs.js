e51dd0f4948c199b0757f10be3842e5f8ab69a09
yujingsu <yuchingsu221@gmail.com>
2021-09-14 01:28:54 +0800

    update diary & some icons

--- src/main/resources/static/diary/js/diaryjs.js
+++ src/main/resources/static/diary/js/diaryjs.js
@@ -9,6 +9,13 @@ $(document).ready(function(){
       
   });
 
+$(".item").on("mouseenter mouseleave", function (moveout) { //挷定滑鼠進入及離開事件
+  if (moveout.type == "mouseenter") {
+    $(this).css({"overflow-y": "scroll"}); //滑鼠進入
+  } else {
+    $(this).scrollTop(0).css({"overflow-y": "hidden"}); //滑鼠離開
+  }
+});
 
 
 
