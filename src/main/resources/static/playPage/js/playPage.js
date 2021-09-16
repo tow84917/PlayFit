444137a6359f2b6ec0b07e04981bcf5f1aa3a6c3
SUPERM4X <tpujp2011@gmail.com>
2021-09-16 15:25:16 +0800

    SQL modification

--- src/main/resources/static/playPage/js/playPage.js
+++ src/main/resources/static/playPage/js/playPage.js
@@ -108,34 +108,21 @@ video.addEventListener("ended", function(){
 // }
 
 
-// // Get the modal
-// var modal = document.getElementById("myModal");
+// Get the modal
+var modal = document.getElementById("myModal");
 
 
-// var buttonLeft = document.getElementsByClassName("modal-left")[0];
 
-// var buttonRight = document.getElementsByClassName("modal-right")[0];
-// // When the user clicks on <span> (x), close the modal
-// buttonRight.onclick = function() {
-//   modal.style.display = "none";
-//   $('#myModal').remove();
-// }
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
 
-// // When the user clicks anywhere outside of the modal, close it
-// buttonLeft.onclick = function() {
-//     $.ajax({
-//         url: "/checkSession" + "/" + fitActivityId + "/" + fitAchieveId,
-//         type: "GET", //send it through POST method
-//         contentType: "application/json;charset=utf-8",
-//         success: function(response) {
-
-            
-//         },
-//         error: function(xhr, textStatus, error) {
-//           alert("fail");
-//           console.log(xhr.statusText);
-//           console.log(textStatus);
-//           console.log(error);
-//           }
-//      });
-// }
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+	modal.style.display = "none";
+  }
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+	if (event.target == modal) {
+	  modal.style.display = "none";
+	}
+  }
