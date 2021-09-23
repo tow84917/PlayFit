73d588de3ba46659dc0e6a391048e9dad233a6ae
SUPERM4X <tpujp2011@gmail.com>
2021-09-23 20:49:14 +0800

    video player page welcome modal

--- src/main/resources/static/playPage/js/playPage.js
+++ src/main/resources/static/playPage/js/playPage.js
@@ -114,7 +114,7 @@ var modal = document.getElementById("myModal");
 
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var span = document.getElementsByClassName("modal-content-bottom-close")[0];
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
